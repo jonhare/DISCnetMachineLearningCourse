416d9eefa6230484e1583868b82990c468642685
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2024-07-04 15:07:30 +0100

    Update galaxy.py

--- Thursday/galaxy.py
+++ Thursday/galaxy.py
@@ -2,6 +2,7 @@ import os
 
 import torch
 from torchvision.transforms import ToTensor
+from torchvision.models import resnet18
 from PIL import Image
 from torch.utils.data import Dataset, random_split
 import pandas as pd
@@ -37,4 +38,29 @@ class GalaxyDataset(Dataset):
 ds = GalaxyDataset("/Users/jsh2/Work/galaxyzoo", transform=ToTensor())
 
 generator = torch.Generator().manual_seed(42)
-random_split(ds, [0.3, 0.3, 0.4], generator=generator)
+train_ds, val_ds = random_split(ds, [0.7, 0.3], generator=generator)
+
+device = "mps"
+model = resnet18(num_classes=37).to(device)
+
+loader = torch.utils.data.DataLoader(train_ds, batch_size=128, shuffle=True)
+learning_rate = 0.01
+
+opt = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(3):
+    losses = 0
+    count = 0
+    for (x_, y_) in loader:
+        opt.zero_grad()
+
+        x_, y_ = x_.to(device), y_.to(device)
+
+        prediction = model(x_)
+        loss = torch.nn.functional.mse_loss(prediction, y_)
+        loss.backward()
+        opt.step()
+
+        losses += loss.detach().cpu().item()
+        count += x_.shape[0]
+        print(losses / count)
